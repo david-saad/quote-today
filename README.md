72606f59f3edbaab4f89db9796a2c13ec2fc8789
David.Boulis <david.boulis@David-Bouliss-MacBook-Pro.local>
2022-06-22 00:34:19 +0200

    feat: quote service with two endponits

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # quote-today
+on-boaring service to introduce k8s and beanstack
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const { quotes } = require('./data');
+
+const PORT = process.env.PORT || 4001;
+
+app.use(express.static('public'));
+app.use(morgan('combined'))
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
+
+// GET a random Quote.
+app.get('/api/quotes/random', (req, res) => {
+  const random = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[random];
+  res.send({ quote: randomQuote });
+});
+
+//GET all quotes or all quotes from an author.
+app.get('/api/quotes', (req, res) => {
+  const filterQuotes = quotes.filter(author => {
+    return author.person === req.query.person;
+  });
+  if (req.query.person) {
+    res.send({ quotes: filterQuotes });
+  } else {
+    res.send({ quotes: quotes });
+  }
+});
+
+//POST for adding new quotes.
+app.post('/api/quotes', (req, res) => {
+  const newQuote = req.query.quote;
+  const newPerson = req.query.person;
+  if (newQuote != '' && newPerson != '') {
+    quotes.push({ quote: newQuote, person: newPerson });
+    res.send({ quote: { quote: newQuote, person: newPerson } });
+  } else {
+    res.sendStatus(400);
+  }
+});
